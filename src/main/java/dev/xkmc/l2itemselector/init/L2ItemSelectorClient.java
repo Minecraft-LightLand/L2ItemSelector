ddfb23c105cf979536bf8e544668998c6aa0a0ce
lcy0x1 <arthurlcy0x1@gmail.com>
2023-07-14 08:07:33 +0800

    fix key registration

--- src/main/java/dev/xkmc/l2itemselector/init/L2ItemSelectorClient.java
+++ src/main/java/dev/xkmc/l2itemselector/init/L2ItemSelectorClient.java
@@ -1,8 +1,10 @@
 package dev.xkmc.l2itemselector.init;
 
+import dev.xkmc.l2itemselector.init.data.L2Keys;
 import dev.xkmc.l2itemselector.select.item.ItemSelectionOverlay;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,6 +15,13 @@ public class L2ItemSelectorClient {
 	@SubscribeEvent
 	public static void registerOverlays(RegisterGuiOverlaysEvent event) {
 		event.registerAbove(VanillaGuiOverlay.CROSSHAIR.id(), "tool_select", ItemSelectionOverlay.INSTANCE);
+	}
+
+	@SubscribeEvent
+	public static void registerKeyMaps(RegisterKeyMappingsEvent event) {
+		for (var e : L2Keys.values()) {
+			event.register(e.map);
+		}
 	}
 
 }
