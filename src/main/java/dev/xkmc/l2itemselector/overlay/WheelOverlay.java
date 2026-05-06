692cb06c020bf4ad65178960cc56f3a9162baa27
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-05-06 13:38:54 +0800

    wheel

--- src/main/java/dev/xkmc/l2itemselector/overlay/WheelOverlay.java
+++ src/main/java/dev/xkmc/l2itemselector/overlay/WheelOverlay.java
@@ -25,15 +25,18 @@ public class WheelOverlay implements LayeredDraw.Layer {
 		float da = (float) (Math.PI * 2 / n);
 		float a0 = (float) (-Math.PI / 2 - da * sel);
 		int x0 = g.guiWidth() / 2, y0 = g.guiHeight() / 2;
-		float r = Math.min(x0, y0) / 2f;
-		float r0 = Math.max(40, r / 2f);
+		float r = Math.min(x0, y0) / 2f; // 轮盘半径
+		float r0 = Math.max(40, r * 0.5f); // 物品渲染位置
+		float r1 = r * 0.25f; //空心部分半径
+		float dr0 = r * 0.025f; // 未选中偏移
+		float dr1 = r * 0.05f; // 选中偏移
 		int ma = WheelHandler.getSel(x0, y0, a0, da, n, r);
 		for (int i = 0; i < n; i++) {
 			float ai = a0 + da * i;
 			if (ma == i) {
-				fillFan(g, x0, y0, ai, da, r, r / 20f, 0, 0x7fffffff);
+				fillFan(g, x0, y0, ai, da, r, r1, dr1, 0, 0x7fffffff);
 			} else {
-				fillFan(g, x0, y0, ai, da, r, r / 40f, 0, 0x3fffffff);
+				fillFan(g, x0, y0, ai, da, r, r1, dr0, 0, 0x3fffffff);
 			}
 			float dx = x0 + Mth.cos(ai) * r0;
 			float dy = y0 + Mth.sin(ai) * r0;
@@ -43,19 +46,20 @@ public class WheelOverlay implements LayeredDraw.Layer {
 	}
 
 
-	public void fillFan(GuiGraphics g, float x0, float y0, float ai, float da, float r0, float dr, int pZ, int col) {
+	public void fillFan(GuiGraphics g, float x0, float y0, float ai, float da, float r0, float r1, float dr, int pZ, int col) {
 		Matrix4f mat = g.pose().last().pose();
 		VertexConsumer vc = g.bufferSource().getBuffer(Shard.GUI_FAN);
 		float x1 = x0 + Mth.cos(ai) * dr;
 		float y1 = y0 + Mth.sin(ai) * dr;
 		int n = (int) Math.max(3, da / (Math.PI / 24));
-		vc.addVertex(mat, x1, y1, pZ).setColor(col);
-
 		for (int i = 0; i <= n; i++) {
 			float a = ai + da / 2 - da / n * i;
 			float x2 = x1 + Mth.cos(a) * r0;
 			float y2 = y1 + Mth.sin(a) * r0;
 			vc.addVertex(mat, x2, y2, pZ).setColor(col);
+			float x3 = x1 + Mth.cos(a) * r1;
+			float y3 = y1 + Mth.sin(a) * r1;
+			vc.addVertex(mat, x3, y3, pZ).setColor(col & 0x00ffffff);
 		}
 
 	}
@@ -68,7 +72,7 @@ public class WheelOverlay implements LayeredDraw.Layer {
 		}
 
 		public static final RenderType GUI_FAN = create("gui_fan",
-				DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.TRIANGLE_FAN, 786432,
+				DefaultVertexFormat.POSITION_COLOR, VertexFormat.Mode.TRIANGLE_STRIP, 786432,
 				RenderType.CompositeState.builder()
 						.setShaderState(RENDERTYPE_GUI_SHADER)
 						.setTransparencyState(TRANSLUCENT_TRANSPARENCY)
